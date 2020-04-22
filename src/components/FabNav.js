2b5890a7b5fae35dea0adbacad13d86492c6f2a3
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-22 16:27:45 -0500

    using state to expand that ul

--- src/components/FabNav.js
+++ src/components/FabNav.js
@@ -6,7 +6,8 @@ import './FabNav.css';
 class FabNav extends React.Component {
   state = {
     clicked: false,
-    height: '0'
+    height: '0',
+    display: 'none'
   }
 
   handleClick = () => {
@@ -14,15 +15,30 @@ class FabNav extends React.Component {
       clicked: !this.state.clicked
     })
     this.state.clicked ? 
-      this.setState({ height: "100%"}) :
-      this.setState({height: "0"})
+      this.setState({ 
+        height: "100%",
+        display: "flex"
+      }) :
+      this.setState({ 
+        height: "0",
+        display: "none"
+      })
   }
 
   render() {
     return (
       <div className="fab-nav-box">
         <div className="background" 
-        style={{height: `${this.state.height}`}}></div>
+        style={{height: `${this.state.height}`}}>
+          <ul 
+            style={{display: `${this.state.display}`}}>
+            <li className="home">Home</li>
+            <li className="about">About</li>
+            <li className="projects">Projects</li>
+            <li className="blog">Blog</li>
+            <li className="contact">Contact</li>
+          </ul>
+        </div>
         <Fab onClick={this.handleClick}>
           <Add />
         </Fab>
