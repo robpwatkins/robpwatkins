0b0088c2826a2c023022edc80790b60d9cb216ad
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-21 21:38:15 -0500

    lets do this.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
