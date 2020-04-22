3ef140661313b659ae51ef78dc6fb1aeaa0cd94e
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-22 13:29:04 -0500

    looks good to me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact, here.</h1>
+    <div className="contact-container">
+      <div className="postcard-container">
+        <div className="postcard">
+          <div className="postcard-left">
+            <div className="mountain-man">
+              <div className="mountain-man-overlay"></div>
+            </div>
+          </div>
+          <div className="postcard-right">
+            <form action="">
+              <input placeholder="name" type="text" />
+              <input placeholder="email" type="text" />
+              <textarea placeholder="message" name="" id="" cols="30" rows="10"></textarea>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../components/Projects.css';
+import './Projects.css';
 
 const Projects = () => {
   return (
