1d6196268a9c2b43d878d920d2d6b2544af5dcfd
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-22 11:46:09 -0500

    about done

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
     <div>
-      <h1>About, here.</h1>
+      <div className="left-box">
+        <div className="left-box-img">
+          <div className="left-box-overlay">
+            <div className="skills">
+              <div className="skill-box skill-box1">
+                <hr className="HTML" />
+                <p>HTML</p>
+              </div>
+              <div className="skill-box skill-box2">
+                <hr className="CSS" />
+                <p>CSS</p>
+              </div>
+              <div className="skill-box skill-box3">
+                <hr className="javaScript" />
+                <p>JavaScript</p>                  
+              </div>
+              <div className="skill-box skill-box4">
+                <hr className="react" />
+                <p>React</p>                  
+              </div>
+              <div className="skill-box skill-box5">
+                <hr className="nodeJS" />
+                <p>NodeJS</p>                  
+              </div>
+              <div className="skill-box skill-box6">
+                <hr className="mySQL" />
+                <p>MySQL</p>                  
+              </div>
+              <div className="skill-box skill-box9">
+                <hr className="github" />
+                <p>Git/Github</p>                  
+              </div>
+              <div className="skill-box skill-box7">
+                <hr className="photoshop" />
+                <p>Photoshop</p>                  
+              </div>
+              <div className="skill-box skill-box10">
+                <hr className="otherSomething" />
+                <p>Something</p>                  
+              </div>
+              <div className="skill-box skill-box8">
+                <hr className="PHP" />
+                <p>PHP</p>                  
+              </div>
+            </div>
+            <div className="mobile-blurbs">
+              <div>
+                <h1>Detail oriented</h1>
+                <p>I take joy in working until the job is clearly done. Precision, simplicity, and eloquence are paramount to my approach.</p>
+              </div>
+              <div>
+                <h1>People person</h1>
+                <p>I thrive in group settings, care deeply about individuals, and always make clear communication a top priority.</p>
+              </div>
+              <div>
+                <h1>Quick comprehension</h1>
+                <p>After 8 months in a bootcamp I am now happily in constant learning mode, ever in search of more elegant ways to solve problems.</p>
+              </div>
+              <div>
+                <h1>Enthusiastic attitude</h1>
+                <p>If I can have fun with it, I will. In everything I do I carry with me a lighthearted positivity and a cheerful spirit.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-box">
+        <div>
+          <h1>Detail oriented</h1>
+          <p>I take joy in working until the job is clearly done. Precision, simplicity, and eloquence are paramount to my approach.</p>
+        </div>
+        <div>
+          <h1>People person</h1>
+          <p>I thrive in group settings, care deeply about individuals, and always make clear communication a top priority.</p>
+        </div>
+        <div>
+          <h1>Quick comprehension</h1>
+          <p>After 8 months in a bootcamp I am now happily in constant learning mode, ever in search of more elegant ways to solve problems.</p>
+        </div>
+        <div>
+          <h1>Enthusiastic attitude</h1>
+          <p>If I can have fun with it, I will. In everything I do I carry with me a lighthearted positivity and a cheerful spirit.</p>
+        </div>
+      </div>
     </div>
   )
 }
