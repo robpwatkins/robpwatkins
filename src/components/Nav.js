d81568391b81b155b6dd5e702c6ae9c12e281d4d
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-24 10:28:16 -0500

    nav and home done for 600

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,28 +2,45 @@ import React from 'react';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
-  return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li className="home">Home</li>
-        </Link>
-        <Link to="/about">
-          <li className="about">About</li>
-        </Link>
-        <Link to="/projects">
-          <li className="projects">Projects</li>
-        </Link>
-        <Link to="/blog">
-          <li className="blog">Blog</li>
-        </Link>
-        <Link to="/contact">
-          <li className="contact">Contact</li>
-        </Link>
-      </ul>
-    </nav>
-  )
+class Nav extends React.Component {
+  state = {
+    color: 'rgba(255, 255, 255, 0.404)'
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ color: 'white' });
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ color: 'rgba(255, 255, 255, 0.404)'})
+  }
+
+  render() {
+    return (
+      <nav 
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        >
+        <ul>
+          <Link to="/" style={{color: `${this.state.color}`}}>
+            <li className="home">Home</li>
+          </Link>
+          <Link to="/about"style={{color: `${this.state.color}`}}>
+            <li className="about">About</li>
+          </Link>
+          <Link to="/projects"style={{color: `${this.state.color}`}}>
+            <li className="projects">Projects</li>
+          </Link>
+          <Link to="/blog"style={{color: `${this.state.color}`}}>
+            <li className="blog">Blog</li>
+          </Link>
+          <Link to="/contact"style={{color: `${this.state.color}`}}>
+            <li className="contact">Contact</li>
+          </Link>
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Nav;
