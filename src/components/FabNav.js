cae3a5ce256f84afe12d36a3702c121526eba52a
Rob W <robpwatkins@gmail.com>
2020-06-14 14:29:24 -0500

    got fab nav using hooks

--- src/components/FabNav.js
+++ src/components/FabNav.js
@@ -1,91 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Fab } from '@material-ui/core';
 import Add from '@material-ui/icons/Add';
 import './FabNav.css';
 import { NavLink } from 'react-router-dom';
 
-class FabNav extends React.Component {
-  state = {
-    clicked: false,
-    height: '0',
-    display: 'none',
-    transform: 'none'
+const FabNav = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [height, setHeight] = useState('0');
+  const [display, setDisplay] = useState('none');
+  const [transform, setTransform] = useState('none');
+  // state = {
+  //   clicked: false,
+  //   height: '0',
+  //   display: 'none',
+  //   transform: 'none'
+  // }
+
+  const handleClick = () => {
+    let newIsClicked = !isClicked;
+    if (newIsClicked) {
+      setHeight('100%')
+      setDisplay('flex')
+      setTransform('rotate(45deg')
+    } else {
+      setHeight('0');
+      setDisplay('none');
+      setTransform('none');
+    }
+    setIsClicked(newIsClicked);
   }
 
-  handleClick = () => {
-    const newClicked = !this.state.clicked;
-    newClicked ? 
-      this.setState({ 
-        height: '100%',
-        display: 'flex',
-        transform: 'rotate(45deg)'
-      }) :
-      this.setState({ 
-        height: '0',
-        display: 'none',
-        transform: 'none'
-      })
-    this.setState({
-      clicked: newClicked
-    })
-  }
-
-  render() {
-    return (
-      <div className="fab-nav-box">
-        <div className="background" 
-          style={{height: `${this.state.height}`}}>
-          <ul 
-            style={{display: `${this.state.display}`}}>
-            <NavLink 
-              exact
-              to="/"
-              onClick={this.handleClick}
-              activeStyle={{transform: "scale(1.5)"}} 
-              >
-              <li>Home</li>
-            </NavLink>
-            <NavLink 
-              to="/about"
-              onClick={this.handleClick}
-              activeStyle={{transform: "scale(1.5)"}} 
-              >
-              <li>About</li>
-            </NavLink>
-            <NavLink 
-              to="/projects"
-              onClick={this.handleClick}
-              activeStyle={{transform: "scale(1.5)"}} 
-              >
-              <li>Projects</li>
-            </NavLink>
-            <NavLink 
-              to="/blog"
-              onClick={this.handleClick}
-              activeStyle={{transform: "scale(1.5)"}} 
-              >
-              <li>Blog</li>
-            </NavLink>
-            <NavLink 
-              to="/contact"
-              onClick={this.handleClick}
-              activeStyle={{transform: "scale(1.5)"}} 
-              >
-              <li>Contact</li>
-            </NavLink>
-          </ul>
-        </div>
-        <Fab className="fabby" onClick={this.handleClick}>
-          <Add className="plusSign" 
-            style={{
-              transform: `${this.state.transform}`,
-              transition: "transform .175s ease-in-out"
-            }}
-          />
-        </Fab>
+  return (
+    <div className="fab-nav-box">
+      <div className="background" 
+        style={{height: `${height}`}}>
+        <ul 
+          style={{display: `${display}`}}>
+          <NavLink 
+            exact
+            to="/"
+            onClick={handleClick}
+            activeStyle={{transform: "scale(1.5)"}} 
+            >
+            <li>Home</li>
+          </NavLink>
+          <NavLink 
+            to="/about"
+            onClick={handleClick}
+            activeStyle={{transform: "scale(1.5)"}} 
+            >
+            <li>About</li>
+          </NavLink>
+          <NavLink 
+            to="/projects"
+            onClick={handleClick}
+            activeStyle={{transform: "scale(1.5)"}} 
+            >
+            <li>Projects</li>
+          </NavLink>
+          <NavLink 
+            to="/blog"
+            onClick={handleClick}
+            activeStyle={{transform: "scale(1.5)"}} 
+            >
+            <li>Blog</li>
+          </NavLink>
+          <NavLink 
+            to="/contact"
+            onClick={handleClick}
+            activeStyle={{transform: "scale(1.5)"}} 
+            >
+            <li>Contact</li>
+          </NavLink>
+        </ul>
       </div>
-    )
-  }
+      <Fab className="fabby" onClick={handleClick}>
+        <Add className="plusSign" 
+          style={{
+            transform: `${transform}`,
+            transition: "transform .175s ease-in-out"
+          }}
+        />
+      </Fab>
+    </div>
+  )
 }
 
 export default FabNav;
