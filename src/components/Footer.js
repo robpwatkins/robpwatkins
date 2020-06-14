9228a8aae159aaadd01d452c8b30764b6bde10bd
Rob W <robpwatkins@gmail.com>
2020-06-14 13:39:28 -0500

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,33 +15,6 @@ const Footer = () => {
     setIsHovered(false);
   }
 
-  // componentDidMount() {
-  //   let currentWindowWidth = window.visualViewport.width;
-  //   window.addEventListener('resize', () => {
-  //     currentWindowWidth = window.visualViewport.width
-  //     if (currentWindowWidth >= 600) {
-  //       this.setState({ 
-  //         color: 'rgba(255, 255, 255, 0.404)',
-  //         windowWidth: currentWindowWidth
-  //       })
-  //     } else {
-  //       this.setState({ 
-  //         color: 'white',
-  //         windowWidth: currentWindowWidth
-  //       })
-  //     }
-  //   });
-  //   if (currentWindowWidth >= 600) {
-  //     this.setState({ 
-  //       color: 'rgba(255, 255, 255, 0.404)' })
-  //     } else {
-  //       this.setState({
-  //         color: 'white'
-  //       })
-  //     }
-  //     this.setState({ windowWidth: currentWindowWidth });
-  // }
-
   return (
     <footer
     >
@@ -49,22 +22,17 @@ const Footer = () => {
         className="footer-box-hover"
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        // style={{color: this.state.color}}
       >
         <div className={!isHovered ? "footer-box" : "footer-box hovered"}>
-          <FontAwesomeIcon icon={faCopyright} className="copyright" />
           <a 
             href="mailto: robpwatkins@gmail.com"
-          >
-            2020 robpwatkins@gmail.com
-          </a>
+            className="mail"
+            >robpwatkins@gmail.com</a>
           <div className="icon-box">
             <a 
               target="_blank"
               rel="noopener noreferrer"
               href="https://www.linkedin.com/in/robpwatkins/"
-              // className={isHovered ? "hovered" : ""}
-              // style={{color: this.state.color}}
             >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
@@ -72,8 +40,6 @@ const Footer = () => {
               target="_blank"
               rel="noopener noreferrer"
               href="https://github.com/robpwatkins"
-              // className={isHovered ? "hovered" : ""}
-              // style={{color: this.state.color}}
             >
               <FontAwesomeIcon icon={faGithub} />
             </a>
@@ -81,8 +47,6 @@ const Footer = () => {
               target="_blank"
               rel="noopener noreferrer"
               href="https://www.instagram.com/robpwatkins/"
-              // className={isHovered ? "hovered" : ""}
-              // style={{color: this.state.color}}
             >
               <FontAwesomeIcon icon={faInstagram} />
             </a>
