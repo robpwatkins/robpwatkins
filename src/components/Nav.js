2c449d6da7e2b19ace659882aa2bc4af576d18f3
Rob W <robpwatkins@gmail.com>
2020-06-14 12:53:25 -0500

    no problem.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,93 +2,85 @@ import React, { useState } from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
 
-class Nav extends React.Component {
-  state = {
-    navHovered: false,
-    color: 'rgba(255, 255, 255, 0.404)',
-  }
+const Nav = () => {
+  const [isHovered, setIsHovered] = useState(false);
 
-  handleMouseEnter = () => {
-    this.setState({ 
-      navHovered: true,
-      color: 'white' 
-    });
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   }
   
-  handleMouseLeave = () => {
-    this.setState({ 
-      navHovered: false,
-      color: 'rgba(255, 255, 255, 0.404)' 
-    });
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   }
 
-  handleMouseMove = () => {
-    this.state.navHovered &&
-    this.setState({ color: 'white' });
-  }
+  // handleMouseMove = () => {
+  //   this.state.navHovered &&
+  //   this.setState({ color: 'white' });
+  // }
 
-  handleClick = () => {
-    this.setState({ color: 'rgba(255, 255, 255, 0.404)' });
-  }
+  // handleClick = () => {
+  //   this.setState({ color: 'rgba(255, 255, 255, 0.404)' });
+  // }
 
-  render() {
-    return (
-      <nav
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onMouseMove={this.handleMouseMove}
-      >
-        <ul>
-          <NavLink 
-            exact 
-            to="/"
-            onClick={this.handleClick}
-            style={{color: `${this.state.color}`}}
-            activeStyle={{color: "white", transform: "scale(1.5)"}}
-            activeClassName="link-active"
-            >
-            <li>Home</li>
-          </NavLink>
-          <NavLink 
-            to="/about"
-            onClick={this.handleClick}
-            style={{color: `${this.state.color}`}}
-            activeStyle={{color: "white", transform: "scale(1.5)"}}
-            activeClassName="link-active"
-            >
-            <li>About</li>
-          </NavLink>
-          <NavLink 
-            to="/projects"
-            onClick={this.handleClick}
-            style={{color: `${this.state.color}`}}
-            activeStyle={{color: "white", transform: "scale(1.5)"}}
-            activeClassName="link-active"
-            >
-            <li>Projects</li>
-          </NavLink>
-          {/* <NavLink 
-            to="/blog"
-            onClick={this.handleClick}
-            style={{color: `${this.state.color}`}}
-            activeStyle={{color: "white", transform: "scale(1.5)"}}
-            activeClassName="link-active"
-            >
-            <li>Blog</li>
-          </NavLink>
-          <NavLink 
-            to="/contact"
-            onClick={this.handleClick}
-            style={{color: `${this.state.color}`}}
-            activeStyle={{color: "white", transform: "scale(1.5"}}
-            activeClassName="link-active"
-            >
-            <li>Contact</li>
-          </NavLink> */}
-        </ul>
-      </nav>
-    )
-  }
+  return (
+    <nav
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      // onMouseMove={this.handleMouseMove}
+    >
+      <ul>
+        <NavLink 
+          exact 
+          to="/"
+          // onClick={handleClick}
+          className={isHovered ? 'nav-link hovered' : 'nav-link'}
+          // style={{color: `${this.state.color}`}}
+          activeStyle={{color: "white", transform: "scale(1.5)"}}
+          activeClassName="link-active"
+          >
+          <li>Home</li>
+        </NavLink>
+        <NavLink 
+          to="/about"
+          // onClick={handleClick}
+          className={isHovered ? 'nav-link hovered' : 'nav-link'}
+          // style={{color: `${this.state.color}`}}
+          activeStyle={{color: "white", transform: "scale(1.5)"}}
+          activeClassName="link-active"
+          >
+          <li>About</li>
+        </NavLink>
+        <NavLink 
+          to="/projects"
+          // onClick={handleClick}
+          className={isHovered ? 'nav-link hovered' : 'nav-link'}
+          // style={{color: `${this.state.color}`}}
+          activeStyle={{color: "white", transform: "scale(1.5)"}}
+          activeClassName="link-active"
+          >
+          <li>Projects</li>
+        </NavLink>
+        {/* <NavLink 
+          to="/blog"
+          onClick={this.handleClick}
+          style={{color: `${this.state.color}`}}
+          activeStyle={{color: "white", transform: "scale(1.5)"}}
+          activeClassName="link-active"
+          >
+          <li>Blog</li>
+        </NavLink>
+        <NavLink 
+          to="/contact"
+          onClick={this.handleClick}
+          style={{color: `${this.state.color}`}}
+          activeStyle={{color: "white", transform: "scale(1.5"}}
+          activeClassName="link-active"
+          >
+          <li>Contact</li>
+        </NavLink> */}
+      </ul>
+    </nav>
+  )
 }
 
 export default Nav;
